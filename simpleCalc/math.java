e80284cdcb4b65e2c477bc7c58248c0ad2a75e77
Luis Fernando Neu <luisneu@gmail.com>
2025-07-29 11:52:51 -0300

    simple calc java

---
+++ simpleCalc/math.java
@@ -0,0 +1,10 @@
+public class math {
+    public static void main(String[] args) {
+        int numero1 = 29;
+        int numero2 = 1;
+
+        int soma = numero1 + numero2;
+
+        System.out.println("A soma de " + numero1 + " + " + numero2 + " é: " + soma);
+    }
+}
